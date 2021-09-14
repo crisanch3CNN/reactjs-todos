672e5322e88a09ef97ba746b8593d24dc3db5840
crisanch <cristian.sanchez@triciclos.cl>
2021-09-14 17:54:33 -0300

    feat(task): add task through

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Fragmet, useState } from "react";
+import React, { Fragmet, useState, useRef } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import { TodoList } from "./components/TodoList";
 
 export function App() {
     const [todos, setTodos] = useState([
         { id: 1, task: "Tarea 1", completed: false },
     ])
+
+    const todoTaskRef = useRef();
+
+    const handleTodoAdd = () => {
+        const task = todoTaskRef.current.value;
+        if (task === '') return;
+        setTodos((prevTodos) => {
+            return [...prevTodos, { id: uuidv4(), task, completed: false }]
+        })
+        todoTaskRef.current.value = null;
+    };
+
     return (
         <>
             <TodoList todos={todos} />
-            <input type="text" placeholder="Nueva Tarea" />
-            <button>add</button>
+            <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea" />
+            <button onClick={handleTodoAdd}>add</button>
             <button>delete</button>
         </>
 
