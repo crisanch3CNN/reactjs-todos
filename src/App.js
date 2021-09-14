7b898a95581d9b4c069f0ec825600fd293b85e52
crisanch <cristian.sanchez@triciclos.cl>
2021-09-14 17:14:37 -0300

    feat(component): add TodoLItem to TodoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { Fragmet, useState } from "react";
 import { TodoList } from "./components/TodoList";
 
 export function App() {
     const [todos, setTodos] = useState([
         { id: 1, task: "Tarea 1", completed: false },
     ])
-    return <TodoList todos={todos} />;
+    return (
+        <>
+            <TodoList todos={todos} />
+            <input type="text" placeholder="Nueva Tarea" />
+            <button>add</button>
+            <button>delete</button>
+        </>
+
+    );
 }
 
